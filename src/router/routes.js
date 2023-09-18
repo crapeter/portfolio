ce7b0ec388312ec6b5aa79758189eb3411cfb8c7
Craig Peterson <139075832+crapeter@users.noreply.github.com>
2023-09-17 20:36:05 -0500

    Delete src directory

--- src/router/routes.js
+++
@@ -1,9 +0,0 @@
-import Home from '../views/Home.vue'
-import Projects from '../views/Projects.vue'
-import ProjectDetails from '../views/ProjectDetails.vue'
-
-export default [
-  { path: '/', component: Home },
-  { path: '/projects', component: Projects },
-  { path: '/projects/:id', component: ProjectDetails }
-]
